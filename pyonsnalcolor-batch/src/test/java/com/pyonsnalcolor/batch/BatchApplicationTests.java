b0ea6582789d2604466720050d5fda658feb17ba
bean.kong <bean.kong@kakaocorp.com>
2023-05-29 01:25:43 +0900

    프로젝트 셋팅

---
+++ pyonsnalcolor-batch/src/test/java/com/pyonsnalcolor/batch/BatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pyonsnalcolor.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ pyonsnalcolor-gateway/src/test/java/com/pyonsnalcolor/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pyonsnalcolor.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
