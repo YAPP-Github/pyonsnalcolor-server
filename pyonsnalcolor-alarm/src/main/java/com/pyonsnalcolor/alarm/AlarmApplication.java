008aa5333aaba8c24525189ea0b7a6d1842217a6
bean.kong <bean.kong@kakaocorp.com>
2023-05-29 01:31:03 +0900

    오타 수정 및 ignore 파일 추가

--- pyonsnalcolor-alarm/src/main/java/com/pyonsnalcolor/alarm/AlarmApplication.java
+++ pyonsnalcolor-alarm/src/main/java/com/pyonsnalcolor/alarm/AlarmApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MemberApplication {
+public class AlarmApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MemberApplication.class, args);
+		SpringApplication.run(AlarmApplication.class, args);
 	}
 
 }
