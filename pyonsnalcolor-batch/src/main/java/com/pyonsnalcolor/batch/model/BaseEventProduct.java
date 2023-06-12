4eaba1ac923880076f5e73f8d5129f78ff9a5dd7
SungBin Hong <31160622+devk0ng@users.noreply.github.com>
2023-06-13 02:01:35 +0900

    공통 처리를 위한 모델 추상화

--- pyonsnalcolor-batch/src/main/java/com/pyonsnalcolor/batch/model/BaseEventProduct.java
+++ pyonsnalcolor-batch/src/main/java/com/pyonsnalcolor/batch/model/BaseEventProduct.java
@@ -1,34 +1,19 @@
 package com.pyonsnalcolor.batch.model;
 
-import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.annotation.Transient;
-import org.springframework.data.mongodb.core.index.Indexed;
+import lombok.experimental.SuperBuilder;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.time.LocalDateTime;
 
-
-@Builder
-@ToString
+@SuperBuilder
+@ToString(callSuper = true)
 @Getter
+@NoArgsConstructor
 @Document(collection = "event_product")
-public class BaseEventProduct {
-//    @Transient
-//    private static final String SEQUENCE_NAME = "event_sequence";
-
-    @Id
-    private String id;
-    @Indexed
-    private StoreType storeType;
-    private String image;
+public class BaseEventProduct extends BaseProduct {
     private EventType eventType;
-    @Indexed
-    private String name;
-    private String price;
     private String originPrice;
     private String giftImage;
-    private LocalDateTime updatedTime;
 }
--- pyonsnalcolor-batch/src/main/java/com/pyonsnalcolor/batch/model/BasePbProduct.java
+++ pyonsnalcolor-batch/src/main/java/com/pyonsnalcolor/batch/model/BasePbProduct.java
@@ -1,30 +1,15 @@
 package com.pyonsnalcolor.batch.model;
 
-import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.annotation.Transient;
-import org.springframework.data.mongodb.core.index.Indexed;
+import lombok.experimental.SuperBuilder;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.time.LocalDateTime;
-
-@Builder
-@ToString
+@SuperBuilder
+@ToString(callSuper = true)
 @Getter
+@NoArgsConstructor
 @Document(collection = "pb_product")
-public class BasePbProduct {
-//    @Transient
-//    private static final String SEQUENCE_NAME = "pb_sequence";
-
-    @Id
-    private String id;
-    @Indexed
-    private StoreType storeType;
-    private String image;
-    @Indexed
-    private String name;
-    private String price;
-    private LocalDateTime updatedTime;
+public class BasePbProduct extends BaseProduct {
 }
