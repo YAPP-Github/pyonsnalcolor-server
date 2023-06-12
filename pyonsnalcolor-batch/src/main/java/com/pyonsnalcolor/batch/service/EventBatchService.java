5112801d01ba874e4f1c8b34d432b28e63d01d41
SungBin Hong <31160622+devk0ng@users.noreply.github.com>
2023-06-13 01:59:49 +0900

    event,pb batch service 구현(batch service template의 상세화)

--- pyonsnalcolor-batch/src/main/java/com/pyonsnalcolor/batch/service/EventBatchService.java
+++ pyonsnalcolor-batch/src/main/java/com/pyonsnalcolor/batch/service/EventBatchService.java
@@ -2,41 +2,16 @@ package com.pyonsnalcolor.batch.service;
 
 import com.pyonsnalcolor.batch.model.BaseEventProduct;
 import com.pyonsnalcolor.batch.repository.EventProductRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
-public abstract class EventBatchService implements BatchService {
-    @Autowired
-    private EventProductRepository eventProductRepository;
-
+public abstract class EventBatchService extends BasicBatchServiceTemplate<BaseEventProduct> {
     public EventBatchService(EventProductRepository eventProductRepository) {
-        this.eventProductRepository = eventProductRepository;
+        super(eventProductRepository);
     }
 
     @Override
-    public void execute() {
-        List<BaseEventProduct> allProducts = getAllProducts();
-        List<BaseEventProduct> newProducts = getNewProducts(allProducts);
-        List<BaseEventProduct> eventExpiredProducts = getEventExpiredProducts(allProducts);
-        sendAlarms(newProducts);
-        saveProducts(newProducts);
-        deleteProducts(eventExpiredProducts);
-    }
-
-    protected abstract List<BaseEventProduct> getAllProducts();
-
-    protected abstract List<BaseEventProduct> getEventExpiredProducts(List<BaseEventProduct> allProducts);
-
-    protected abstract List<BaseEventProduct> getNewProducts(List<BaseEventProduct> allProducts);
-
-    protected abstract void sendAlarms(List<BaseEventProduct> baseProducts);
-
-    private final void saveProducts(List<BaseEventProduct> baseProducts) {
-        eventProductRepository.saveAll(baseProducts);
-    }
-
-    private final void deleteProducts(List<BaseEventProduct> baseProducts) {
-        eventProductRepository.deleteAll(baseProducts);
+    protected void deleteProducts(List<BaseEventProduct> baseProducts) {
+        basicProductRepository.deleteAll(baseProducts);
     }
 }
