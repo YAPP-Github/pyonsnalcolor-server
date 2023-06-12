181493eb50277e1cbedc9dea5a8bd450d8446262
SungBin Hong <31160622+devk0ng@users.noreply.github.com>
2023-06-13 02:00:58 +0900

    repository level 리팩토링 (추상화 한단계 추가)

--- pyonsnalcolor-batch/src/main/java/com/pyonsnalcolor/batch/repository/EventProductRepository.java
+++ pyonsnalcolor-batch/src/main/java/com/pyonsnalcolor/batch/repository/EventProductRepository.java
@@ -1,9 +1,8 @@
 package com.pyonsnalcolor.batch.repository;
 
 import com.pyonsnalcolor.batch.model.BaseEventProduct;
-import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EventProductRepository extends MongoRepository<BaseEventProduct, Long> {
+public interface EventProductRepository extends BasicProductRepository<BaseEventProduct, String> {
 }
