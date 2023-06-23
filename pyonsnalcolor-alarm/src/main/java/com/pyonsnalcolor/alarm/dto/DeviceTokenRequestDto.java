58dd7c59149d42f95d560d91a2a3c8d33fa0327a
sooyoungh <sooyoung.h8@gmail.com>
2023-06-24 04:06:47 +0900

    feat: FCM 환경 설정, 테스트용 구현

---
+++ pyonsnalcolor-alarm/src/main/java/com/pyonsnalcolor/alarm/dto/DeviceTokenRequestDto.java
@@ -0,0 +1,15 @@
+package com.pyonsnalcolor.alarm.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class DeviceTokenRequestDto {
+
+    private String deviceToken;
+}
