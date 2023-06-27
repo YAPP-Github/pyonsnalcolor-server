b8f7a7ef9a3d0bc68523afb5441c59a12261438f
sooyoungh <sooyoung.h8@gmail.com>
2023-06-28 08:40:17 +0900

    build: jar 옵션 설정

--- pyonsnalcolor-member/src/main/java/com/pyonsnalcolor/auth/controller/AuthController.java
+++ pyonsnalcolor-member/src/main/java/com/pyonsnalcolor/auth/controller/AuthController.java
@@ -10,10 +10,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -49,5 +46,10 @@ public class AuthController {
     ) {
         TokenDto newTokenDto = memberService.reissueAccessToken(tokenDto);
         return new ResponseEntity(newTokenDto, HttpStatus.OK);
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<String> test() {
+        return new ResponseEntity("Welcome to Pyonsnal Color", HttpStatus.OK);
     }
 }
