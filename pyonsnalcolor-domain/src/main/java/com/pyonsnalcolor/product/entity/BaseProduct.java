fb3c730a54ccf5b730d1450efc1348139503422e
sooyoungh <sooyoung.h8@gmail.com>
2023-06-30 20:04:25 +0900

    fix: LocalDateTime 직렬화 오류 해결

--- pyonsnalcolor-domain/src/main/java/com/pyonsnalcolor/product/entity/BaseProduct.java
+++ pyonsnalcolor-domain/src/main/java/com/pyonsnalcolor/product/entity/BaseProduct.java
@@ -2,7 +2,9 @@ package com.pyonsnalcolor.product.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.pyonsnalcolor.product.enumtype.StoreType;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -27,6 +29,7 @@ public class BaseProduct {
     private String name;
     private String price;
     @JsonFormat(pattern = "yyyy-MM-dd")
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
     @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime updatedTime;
 }
