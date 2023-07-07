b5dd5c64e1e97b39126bc6197a580119b4ccd740
sooyoungh <sooyoung.h8@gmail.com>
2023-07-07 11:29:58 +0900

    feat: 푸시 편의점별 구독 관련 기능 테스트

---
+++ pyonsnalcolor-api/src/test/java/com/pyonsnalcolor/push/service/PushProductStoreServiceTest.java
@@ -0,0 +1,97 @@
+package com.pyonsnalcolor.push.service;
+
+import com.pyonsnalcolor.auth.AuthUserDetails;
+import com.pyonsnalcolor.auth.service.MemberService;
+import com.pyonsnalcolor.member.Member;
+import com.pyonsnalcolor.member.MemberRepository;
+import com.pyonsnalcolor.member.enumtype.OAuthType;
+import com.pyonsnalcolor.member.enumtype.Role;
+import com.pyonsnalcolor.push.dto.PushProductStoreRequestDto;
+import com.pyonsnalcolor.push.dto.PushProductStoreResponseDto;
+import com.pyonsnalcolor.push.repository.PushProductStoreRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@Transactional
+@SpringBootTest
+class PushProductStoreServiceTest {
+
+    @Autowired
+    private PushProductStoreService pushProductStoreService;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @DisplayName("회원가입하면 모든 편의점을 구독 신청합니다.")
+    @Test
+    void getPushProductStores() {
+        Member member =  Member.builder()
+                .email("sample@gmail.com")
+                .OAuthType(OAuthType.APPLE)
+                .oAuthId("apple-sample@gmail.com")
+                .refreshToken("refreshToken")
+                .role(Role.ROLE_USER).build();
+        memberRepository.save(member);
+        pushProductStoreService.createPushProductStores(member);
+        AuthUserDetails authUserDetails = getAuthentication(member);
+
+        List<PushProductStoreResponseDto> result = pushProductStoreService.getPushProductStores(authUserDetails);
+
+        assertAll(
+                () -> assertThat(result.size()).isEqualTo(8),
+                () -> assertThat(result).extracting("productStore")
+                        .contains("PB_CU", "PB_GS25", "PB_EMART24", "PB_SEVEN_ELEVEN",
+                                "EVENT_CU", "EVENT_GS25", "EVENT_EMART24", "EVENT_SEVEN_ELEVEN"),
+                () -> assertThat(result).extracting("isSubscribed")
+                        .containsOnly(true));
+    }
+
+    @DisplayName("편의점 푸시 알림을 구독 취소합니다.")
+    @Test
+    void subscribePushProductStores() {
+        Member member =  Member.builder()
+                .email("sample@gmail.com")
+                .OAuthType(OAuthType.APPLE)
+                .oAuthId("apple-sample@gmail.com")
+                .refreshToken("refreshToken")
+                .role(Role.ROLE_USER).build();
+        memberRepository.save(member);
+        pushProductStoreService.createPushProductStores(member);
+        AuthUserDetails authUserDetails = getAuthentication(member);
+
+        PushProductStoreRequestDto requestDto = PushProductStoreRequestDto.builder()
+                .pushProductStores(List.of("EVENT_CU", "PB_CU"))
+                .build();
+
+        pushProductStoreService.unsubscribePushProductStores(authUserDetails, requestDto);
+        List<PushProductStoreResponseDto> result = pushProductStoreService.getPushProductStores(authUserDetails);
+
+        assertAll(
+                () -> assertThat(result).filteredOn(PushProductStoreResponseDto::getProductStore, "PB_CU")
+                        .filteredOn(PushProductStoreResponseDto::getIsSubscribed, false),
+                () -> assertThat(result).filteredOn(PushProductStoreResponseDto::getProductStore,"EVENT_CU")
+                        .filteredOn(PushProductStoreResponseDto::getIsSubscribed, false));
+    }
+
+    // CustomUserDetails로 Member 객체를 매핑하기 때문에 필요 - 이후 수정
+    private AuthUserDetails getAuthentication(Member member){
+        SecurityContext context = SecurityContextHolder.getContext();
+        AuthUserDetails authUserDetails = new AuthUserDetails(member);
+        context.setAuthentication(new UsernamePasswordAuthenticationToken(
+                authUserDetails,
+                "",
+                authUserDetails.getAuthorities()));
+        return authUserDetails;
+    }
+}
