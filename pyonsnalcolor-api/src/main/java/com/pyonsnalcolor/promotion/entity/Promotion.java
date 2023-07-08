613baecea9c86ca962cdc5d16707d5e096cde7c9
devk0ng <kongbean@kakao.com>
2023-07-08 22:16:20 +0900

    행사 프로모션 조회 api 구현

--- pyonsnalcolor-api/src/main/java/com/pyonsnalcolor/promotion/entity/Promotion.java
+++ pyonsnalcolor-api/src/main/java/com/pyonsnalcolor/promotion/entity/Promotion.java
@@ -1,9 +1,9 @@
 package com.pyonsnalcolor.promotion.entity;
 
 import com.pyonsnalcolor.product.enumtype.StoreType;
+import com.pyonsnalcolor.promotion.dto.PromotionResponseDto;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.ToString;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
@@ -24,4 +24,16 @@ public class Promotion {
     private String image;
     private String title;
     private LocalDateTime updatedTime;
+
+    public PromotionResponseDto convertToDto() {
+        PromotionResponseDto promotionResponseDto = new PromotionResponseDto();
+        promotionResponseDto.setId(id);
+        promotionResponseDto.setStoreType(storeType);
+        promotionResponseDto.setThumbnailImage(thumbnailImage);
+        promotionResponseDto.setImage(image);
+        promotionResponseDto.setTitle(title);
+        promotionResponseDto.setUpdatedTime(updatedTime);
+
+        return promotionResponseDto;
+    }
 }
