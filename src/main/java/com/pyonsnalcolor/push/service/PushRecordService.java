64d60b266b7f8079c57818b5f982384c3494c8d7
sooyoungh <sooyoung.h8@gmail.com>
2023-07-22 02:00:30 +0900

    chore: 패키지 변경

---
+++ src/main/java/com/pyonsnalcolor/push/service/PushRecordService.java
@@ -0,0 +1,12 @@
+package com.pyonsnalcolor.push.service;
+
+import com.pyonsnalcolor.push.repository.PushRecordRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PushRecordService {
+
+    @Autowired
+    PushRecordRepository pushRecordRepository;
+}
---
+++ src/test/java/com/pyonsnalcolor/product/service/EventProductServiceTest.java
@@ -0,0 +1,10 @@
+package com.pyonsnalcolor.product.service;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@SpringBootTest
+class EventProductServiceTest {
+
+}
