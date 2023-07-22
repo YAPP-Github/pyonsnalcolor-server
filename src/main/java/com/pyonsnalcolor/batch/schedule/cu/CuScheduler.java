37f0c49db476affff6b941ed882eb89f6595a133
devk0ng <kongbean@kakao.com>
2023-07-22 16:19:08 +0900

    오타 수정 및 cu, seven scheduling 등록

--- src/main/java/com/pyonsnalcolor/batch/schedule/cu/CuScheduler.java
+++ src/main/java/com/pyonsnalcolor/batch/schedule/cu/CuScheduler.java
@@ -10,8 +10,8 @@ import org.springframework.stereotype.Component;
 public class CuScheduler extends Scheduler {
     @Autowired
     public CuScheduler(@Qualifier("CuPb") BatchService gs25PbBatchService,
-                       @Qualifier("CuEvent") BatchService gs25EventBatchService) {
-        //TODO : 추가 필요
-        super(gs25PbBatchService, gs25EventBatchService, null);
+                       @Qualifier("CuEvent") BatchService gs25EventBatchService,
+                        @Qualifier("CuPromotion") BatchService cuPromotionBatchService) {
+        super(gs25PbBatchService, gs25EventBatchService, cuPromotionBatchService);
     }
 }
--- src/main/java/com/pyonsnalcolor/batch/schedule/seven/SevenScheduler.java
+++ src/main/java/com/pyonsnalcolor/batch/schedule/seven/SevenScheduler.java
@@ -9,9 +9,9 @@ import org.springframework.stereotype.Component;
 @Component
 public class SevenScheduler extends Scheduler {
     @Autowired
-    public SevenScheduler(@Qualifier("SevenPb") BatchService gs25PbBatchService,
-                          @Qualifier("SevenEvent") BatchService gs25EventBatchService) {
-        //TODO : 추가 필요
-        super(gs25PbBatchService, gs25EventBatchService, null);
+    public SevenScheduler(@Qualifier("SevenPb") BatchService sevenPbBatchService,
+                          @Qualifier("SevenEvent") BatchService sevenEventBatchService,
+                          @Qualifier("SevenPromotion") BatchService sevenPromotionBatchService) {
+        super(sevenPbBatchService, sevenEventBatchService, sevenPromotionBatchService);
     }
 }
