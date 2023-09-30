b3e89912466a352687b5a8e55327ef34c8e9b869
devk0ng <kongbean@kakao.com>
2023-09-30 13:46:37 +0900

    review 생성 업데이트 시간 기록

--- src/main/java/com/pyonsnalcolor/product/dto/ReviewDto.java
+++ src/main/java/com/pyonsnalcolor/product/dto/ReviewDto.java
@@ -1,9 +1,16 @@
 package com.pyonsnalcolor.product.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.pyonsnalcolor.product.enumtype.Like;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
@@ -17,4 +24,8 @@ public class ReviewDto {
     private String image;
     private Long writerId; //작성자 <- 이후 기능 확장 고려한 필드
     private String writerName; // 닉네임
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    private LocalDateTime updatedTime;
 }
